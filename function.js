b81ab8fd5d484ec57bbd9b1eca0c073943eea299
joyoanta <banik.joyoanta12@gmail.com>
2020-07-30 14:30:47 +0600

    BOTH BUTTON WORKED

--- function.js
+++ function.js
@@ -1,10 +1,11 @@
-// + button event handeler 
+// product one  + button event handeler 
 
 var productAdd = document.getElementById("product-add");
 productAdd.addEventListener("click", function(){
 
   var valueAdd =  document.getElementById("valueAdd").value;
   var valueNumber = parseFloat (valueAdd);
+
   var newValueNumber = valueNumber + 1 ; 
   document.getElementById("valueAdd").value = newValueNumber; 
 
@@ -20,19 +21,74 @@ productAdd.addEventListener("click", function(){
 })
 
 
-// - button event handeler 
+// product one - button event handeler 
 
 var productRemove = document.getElementById("productRemove");
 productRemove.addEventListener("click", function(){
 
   var valueAdd =  document.getElementById("valueAdd").value;
   var valueNumber = parseFloat (valueAdd);
-  var newValueNumber = valueNumber - 1 ; 
+  var newValueNumber = valueNumber + (-1) ; 
   document.getElementById("valueAdd").value = newValueNumber; 
 
   var productOne =  document.getElementById("productOne").innerText
   productOneNumber = parseFloat(productOne);
  
-  var productOnePrice = productOneNumber -1219;
+  var productOnePrice = productOneNumber - 1219;
   document.getElementById("productOne").innerText = productOnePrice;
 })
+
+
+// Product two + button event handeler
+
+var productPlus = document.getElementById("productPlus");
+productPlus.addEventListener("click", function(){
+
+  var valueAdd =  document.getElementById("valueChange").value;
+  var valueNumber = parseFloat (valueAdd);
+
+  var newValueNumber = valueNumber + 1 ; 
+  document.getElementById("valueChange").value = newValueNumber; 
+
+  
+
+ var productTwo =  document.getElementById("productTwo").innerText
+ productOneNumber = parseFloat(productTwo);
+
+ var productTwoPrice = 59 * newValueNumber
+ document.getElementById("productTwo").innerText = productTwoPrice;
+
+
+})
+
+
+// Product two - button event handeler
+
+var productMinus = document.getElementById("productMinus");
+productMinus.addEventListener("click", function(){
+
+  var valueChange =  document.getElementById("valueChange").value;
+  var valueNumber = parseFloat (valueChange);
+  var newValueNumber = valueNumber + (-1) ; 
+  document.getElementById("valueChange").value = newValueNumber; 
+
+  var productTwo =  document.getElementById("productTwo").innerText
+  productTwoNumber = parseFloat(productTwo);
+ 
+  var productTwoPrice = productTwoNumber - 59;
+  document.getElementById("productTwo").innerText = productTwoPrice;
+})
+
+
+// subtotal 
+
+
+
+
+
+
+
+
+
+
+
