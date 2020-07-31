3a5701492718a360d8d9e897f75775cba73d667a
joyoanta <banik.joyoanta12@gmail.com>
2020-07-31 21:22:43 +0600

    sub total, tax, grand total calculated

--- function.js
+++ function.js
@@ -68,35 +68,61 @@ document.getElementById("phone-decrease").addEventListener('click', function(){
 // }
 
 
+function subCalculate() {
+var phoneInput =  document.getElementById("phone-count");
+var phoneCount =  parseInt (phoneInput.value);
+
+var caseInput = document.getElementById("case-count");  
+var caseCount =  parseInt (caseInput.value);
+
+var subTotal = phoneCount * 1219 + caseCount * 59 ; 
+
+document.getElementById("sub-total").innerText = subTotal;
+
+var tax = subTotal * 0.05
+var taxAmount = Math.round(tax);
+
+var grandTotal = subTotal + tax ; 
+var grandAmount = Math.round(grandTotal)
+
+document.getElementById("tax").innerText = taxAmount; 
+document.getElementById("total").innerText = grandAmount; 
+
+}
+
 
 function productChange (product, isIncrease){
-
   var productInput = document.getElementById(product +'-count');
   var productCount = parseInt(productInput.value);
-  
   var productNewCount = productCount;
-  if (isIncrease == true) {
-    productNewCount = productCount + 1 ;
-  }
 
-  if (isIncrease == false && productCount> 0){
-    productNewCount = productCount - 1 ;
-  }
+    if (isIncrease == true) {
+      productNewCount = productCount + 1 ;
+    }
 
-productInput.value = productNewCount; 
+    if (isIncrease == false && productCount> 0){
+      productNewCount = productCount - 1 ;
+    }
 
+  productInput.value = productNewCount; 
   var productTotal = 0; 
 
-  if (product == "phone") {
-    var productTotal = productNewCount * 1219 ; 
-  } 
+    if (product == "phone") {
+      var productTotal = productNewCount * 1219 ; 
+    } 
 
-  if (product == "case"){
-    var productTotal = productNewCount * 59 ; 
-  }
+    if (product == "case"){
+      var productTotal = productNewCount * 59 ; 
+    }
 
   document.getElementById(product +"-total").innerText = productTotal; 
+  subCalculate()
+  
+
 }
+
+
+
 
 
 
